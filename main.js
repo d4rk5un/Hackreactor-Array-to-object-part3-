1a31c9906ef5bd38749239c9d006c8556a4ce509
Kiril Kostov <kiril.kostov@live.com>
2017-09-02 10:06:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var array = [
+    [
+        ['firstName', 'Joe'], ['lastName', 'Blow'], ['age', 42], ['role', 'clerk']
+    ],
+    [
+        ['firstName', 'Mary'], ['lastName', 'Jenkins'], ['age', 36], ['role', 'manager']
+    ]
+];
+
+/*Expected output:
+[
+    {firstName: 'Joe', lastName: 'Blow', age: 42, role: 'clerk'},
+    {firstName: 'Mary', lastName: 'Jenkins', age: 36, role: 'manager'}
+]
+*/
+
+var obj = obj = array[0];;
+var obj2 = obj2 = array[1];
+var arr = [];
+
+var x = obj.reduce(function(acc, cur) {
+  acc[cur[0]] = cur[1];
+  return acc;
+}, {});
+
+var y = obj2.reduce(function(acc, cur) {
+  acc[cur[0]] = cur[1];
+  return acc;
+}, {});
+
+arr.push(x, y);
+console.log(arr);
